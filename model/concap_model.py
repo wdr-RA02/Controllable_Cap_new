7e405bd35c31de9815b078a47a453c16d8a50b95
wdr-RA02 <cydia1829@gmail.com>
2023-09-22 18:06:48 +0800

    migrate concap model to prompt_tuning

--- model/concap_model.py
+++ model/concap_model.py
@@ -223,3 +223,151 @@ class ConCapModel(BlipPreTrainedModel):
         )
 
         return outputs
+    
+
+class ConCapModelNew(BlipPreTrainedModel):
+    config_class=BlipConfig
+
+    def __init__(self, 
+                 config: BlipConfig, 
+                 pfx_config_dict: dict,
+                 *inputs,
+                 **kwargs):
+        
+        super().__init__(config, *inputs, **kwargs)
+        self.config=config
+        # decoder for LM
+        self.text_decoder=BlipTextLMHeadModel(config.text_config)
+        # vision model
+        self.vision_model=BlipVisionModel(config.vision_config)
+        self.init_prefix(pfx_config_dict)
+
+        # [ENC] and [BOS]
+        # self.enc_token_id=getattr(config.text_config, "enc_token_id", 30523)
+        self.decoder_bos_id=config.text_config.bos_token_id
+        self.pad_token_id=config.text_config.pad_token_id
+
+        # post init work
+        self.post_init()
+
+    
+    def init_prefix(self, pfx_dict: dict):
+        self.prefix_hid=pfx_dict["prefix_hidden_size"]
+        self.pfx_len=pfx_dict["prefix_len"] 
+        self.n_style=pfx_dict["n_cls"]
+        self.prefix_proj=pfx_dict["prefix_projection"]
+
+        n_emb=self.pfx_len*self.n_style
+        dec_hidden=self.config.text_config.hidden_size
+
+        # init prefix
+        # [b, pfx_len] => [b, pfx_len, hidden]
+        self.decoder_prefix=torch.nn.Embedding(n_emb, dec_hidden)
+    
+
+    def embed_from_ids(self, input_ids, prefix_ids):
+        # convert prefix_ids to prefix_embs, shape=[b, pfx_len, hidden]
+        prefix_embs=self.decoder_prefix(prefix_ids)
+
+        # input_ids -> input_embeds, shape=[b, seq_len, hidden]
+        input_embs=self.text_decoder.get_input_embeddings()(input_ids)
+        # concat
+        input_embs=torch.concat([prefix_embs, input_embs], dim=1).to(input_embs.device)
+
+        return input_embs
+    
+
+    def forward(self, 
+                input_ids, 
+                attention_mask, 
+                pixel_values, 
+                labels=None, 
+                past_key_values=None,
+                prefix_ids=None
+        )->dict:
+        
+        batch, pfx_len=prefix_ids.shape
+        # ---------------Obtain image feature using ViT Encoder---------------
+        vision_outputs=self.vision_model(pixel_values)
+        img_embeds=vision_outputs[0]
+        img_attn=torch.ones(img_embeds.shape[:-1], dtype=torch.long)
+
+        #-----------------------LM----------------------------
+        lm_input_ids=input_ids.clone()
+        # LM starts with [BOS]
+        lm_input_ids[:,0]=self.decoder_bos_id
+
+        # input_ids -> input_embeds, shape=[b, seq_len, hidden]
+        input_embs=self.embed_from_ids(input_ids=input_ids, prefix_ids=prefix_ids)
+        # also concat attention mask (ie: ones w/ [b, pfx_len])
+        pfx_attn_mask=torch.ones((batch, pfx_len)).to(input_embs.device)
+        attn_mask=torch.cat([pfx_attn_mask, attention_mask], dim=1).to(attention_mask.device)
+
+        text_lm_output=self.text_decoder(
+            inputs_embeds=input_embs,
+            attention_mask=attn_mask,
+            past_key_values=past_key_values,
+            encoder_hidden_states=img_embeds,
+            encoder_attention_mask=img_attn,
+            reduction="mean",
+            return_dict=True
+        )
+
+        # calculate loss from the logits w.o. prefix logits
+        dec_logits=text_lm_output.logits[:, pfx_len:, :]
+        loss_lm = None
+
+        if labels is not None:
+            # mask fill
+            labels=labels.masked_fill(labels==self.pad_token_id, -100)
+            # next-word prediction CE loss
+            shift_logits=dec_logits[:, :-1, :].contiguous()
+            labels=labels[:,1:].contiguous().to(shift_logits.device)
+            loss_lm=torch.nn.CrossEntropyLoss(label_smoothing=0.1)(
+                shift_logits.view(-1, self.config.text_config.vocab_size),
+                labels.view(-1)
+            )
+        
+        return {"loss_lm": loss_lm,
+                "img_embeds": img_embeds,
+                "dec_logits": dec_logits}
+
+    @torch.no_grad()
+    def generate(self, 
+                 pixel_values, 
+                 input_ids=None, 
+                 prefix_ids=None, 
+                 attention_mask=None, 
+                 **generate_kwargs):
+        # Img feature
+        batch_size = pixel_values.shape[0]
+        vision_outputs = self.vision_model(
+            pixel_values=pixel_values
+        )
+
+        image_embeds=vision_outputs[0]
+        image_attention_mask = torch.ones(image_embeds.size()[:-1], 
+                                          dtype=torch.long).to(image_embeds.device)
+        
+        # prepare input ids
+        if isinstance(input_ids, list):
+            input_ids = torch.LongTensor(input_ids)
+        elif input_ids is None:
+            input_ids = (
+                torch.LongTensor([[self.decoder_bos_id, self.config.text_config.eos_token_id]])
+                .repeat(batch_size, 1)
+                .to(image_embeds.device)
+            )
+
+        input_ids[:, 0] = self.config.text_config.bos_token_id
+        attention_mask = attention_mask[:, :-1] if attention_mask is not None else None
+
+        # get embs
+        input_embs=self.embed_from_ids(input_ids=input_ids, prefix_ids=prefix_ids)
+        batch, pfx_len=prefix_ids.shape
+        # TODO: concat attn mask with prefix all_one
+        pfx_attn_mask=torch.ones((batch, pfx_len)).to(input_embs.device)
+        attn_mask=torch.cat([pfx_attn_mask, attention_mask], dim=1).to(attention_mask.device)
+
+        # TODO: impl the rest of generate() with beam gen
+
